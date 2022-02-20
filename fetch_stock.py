53e4d7cd5e437e61ca4c8e871fd166e5ec4550b9
deguchi <deguchi.k.110@gmail.com>
2022-02-21 00:11:50 +0900

    コード入力に対するレスポンスに変更

--- fetch_stock.py
+++ fetch_stock.py
@@ -1,22 +1,25 @@
 import yfinance as yf
+import re
 
 def fetch_company_info(response_body, company_id):
   ticker = yf.Ticker(f"{company_id}.T")
   ticker_info = ticker.info
 
+  response_body.append(f"コード: {company_id}")
   response_body.append(f"企業名: {ticker_info['longName']}")
   current_price = "{:,}".format(ticker_info['currentPrice'])
   response_body.append(f"現在値: { current_price }円")
   return
 
-def fetch_stock():
-  # 9984: Softbank group, T: 東証
-  # 7974: Nintendo
-  company_ids = [9984, 7974]
+def fetch_stock(input_ids):
+  company_ids = re.sub(r"\s+", "", input_ids).split(',')
   response_body = []
 
   # fetch each company info
   for company_id in company_ids:
-    fetch_company_info(response_body, company_id)
+    if company_id.isdecimal():
+      fetch_company_info(response_body, int(company_id))
+    else:
+      response_body.append(f"error: {company_id} is not existed code.")
 
   return '\n'.join(response_body)
--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def callback():
 def handle_message(event):
   line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=fetch_stock())
+    TextSendMessage(text=fetch_stock(event.message.text))
   )
 
 if __name__ == '__main__':
